5968caf52b6f3d3fc33bb42944769b30b8c3aaa0
gyermolenko <gyermolenko@gmail.com>
2016-03-20 18:37:41 +0200

    Migrate from sqlite3 to local mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'testuser2',
+        'PASSWORD': 'testpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
